50c2c38ea22d49ca25718f3919d034334b3d9ec0
kylariaaa <shikitao900@gmail.com>
2026-01-26 21:55:35 +0700

    U

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('auth.login');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Jalur Tamu (Belum Login)
